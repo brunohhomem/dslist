ab524abadcd45f62ae200c936cfe2de2959282db
Bruno H. Homem <brunohhomem@gmail.com>
2024-01-06 14:26:25 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -2,5 +2,4 @@ package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface GameRepository extends JpaRepository<Game, Long> {}
